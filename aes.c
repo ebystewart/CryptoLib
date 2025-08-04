9c0155c447f20c86e8972855af6ce46a43a19bc1
Eby Stewart <ebyystewart@gmail.com>
2025-08-04 23:58:53 +0530

    few static utility functions

---
+++ aes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "aes.h"
+
+/* Static functions */
+int aes_get_round_key(char *key_in, char *round_key);
+
+int aes_create_s_box(char *in);
+
+int aes_substitute_bytes(char *in, char *out);
+
+int aes_shift_rows(char *in, char *out);
+
+int aes_mix_columns(char *in, char *out);
+
+int aes_get_round_key(char *key_in, char *round_key)
+{
+
+}
+
+int aes_create_s_box(char *in)
+{
+
+}
+
+int aes_substitute_bytes(char *in, char *out)
+{
+
+}
+
+int aes_shift_rows(char *in, char *out)
+{
+
+}
+
+int aes_mix_columns(char *in, char *out)
+{
+    
+}
