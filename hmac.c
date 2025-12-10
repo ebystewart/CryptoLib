e3e4e142116fb86945b9d778b92ef3797d154be6
Eby Stewart <ebyystewart@gmail.com>
2025-12-10 23:37:12 +0530

    TLS 1.3: handshake keys generation API - under development

---
+++ hmac.c
@@ -0,0 +1,11 @@
+#include "hmac.h"
+
+void hmac_hkdf_extract(const uint8_t *salt, uint32_t saltLen, const uint8_t *keyIn, uint32_t keyInLen, uint8_t *keyOut, uint32_t *keyOutLen)
+{
+
+}
+
+void hmac_hkdf_expand_label(const uint8_t *keyIn, uint32_t keyInLen, const char *label, const uint8_t *ctx_hash, const uint32_t len, uint8_t *keyOut)
+{
+    
+}
--- hmac.h
+++ hmac.h
@@ -1,5 +1,10 @@
 #ifndef _HMAC_H_
 #define _HMAC_H_
 
+#include <stdint.h>
+
+void hmac_hkdf_extract(const uint8_t *salt, uint32_t saltLen, const uint8_t *keyIn, uint32_t keyInLen, uint8_t *keyOut, uint32_t *keyOutLen);
+
+void hmac_hkdf_expand_label(const uint8_t *keyIn, uint32_t keyInLen, const char *label, const uint8_t *ctx_hash, const uint32_t len, uint8_t *keyOut);
 
 #endif
